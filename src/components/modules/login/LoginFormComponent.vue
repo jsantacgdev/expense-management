bc849631bb3e2725a3bba1b9c806f5108c36bc12
JASantacruz00 <jsantacgdev@gmail.com>
2024-09-20 21:20:13 +0200

    Login View

---
+++ src/components/modules/login/LoginFormComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="flex flex-col items-center align-middle justify-center w-1/2 shadow-lg"
+  >
+    <Card class="w-full h-full">
+      <template v-slot:header>
+        <h1 class="text-center mt-3 font-bold text-2xl">Cashpilot</h1>
+      </template>
+      <template v-slot:content>
+        <div class="flex flex-col items-center justify-center">
+          <InputText v-model="usuario" label="Usuario" class="mb-10" />
+          <InputPassword v-model="password" label="Contraseña" />
+          <Button label="Entrar" class="w-48 mt-5" />
+        </div>
+      </template>
+      <template v-slot:footer>
+        <div class="flex flex-col items-center justify-center">
+          <span class="text-center text-sm">
+            ¿No tienes cuenta?
+            <router-link to="/register" class="text-blue-500">
+              Registrate
+            </router-link>
+          </span>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type Ref, ref } from "vue";
+import InputText from "@shared/components/InputText.vue";
+import Button from "primevue/button";
+import InputPassword from "@/shared/components/InputPassword.vue";
+import Card from "primevue/card";
+
+const usuario: Ref<string> = ref("");
+const password: Ref<string> = ref("");
+</script>
+
+<style scoped>
+.background img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  filter: brightness(0.5);
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
       name: "home",
       component: MainView,
     },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
   ],
 });
 

5ee28a60be1c2acd70699a4c45e13211e2e28392
JASantacruz00 <jsantacgdev@gmail.com>
2024-10-01 23:19:15 +0200

    DataTable Component

---
+++ src/shared/components/DataTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <DataTable
+      :value="items"
+      tableStyle="min-width: 50rem"
+      :stripedRows="stripedRows"
+    >
+      <Column
+        v-for="column in columns"
+        :key="column.field"
+        :field="column.field"
+        :header="column.header"
+        :sortable="column.sortable"
+      >
+      </Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row"; // optional
+import { type IColumn } from "../interfaces/column";
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+  columns: {
+    type: Array as () => IColumn[],
+    required: true,
+  },
+  stripedRows: {
+    type: Boolean,
+    default: true,
+  },
+  pagination: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>

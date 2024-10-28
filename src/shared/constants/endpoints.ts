be256a9eb392674c6a68a05cae1b2b3196a9f101
JASantacruz00 <jsantacgdev@gmail.com>
2024-10-28 19:39:16 +0100

    Adding endpoints

--- src/shared/constants/endpoints.ts
+++ src/shared/constants/endpoints.ts
@@ -2,5 +2,7 @@ export const EEndpoints = {
   LOGIN: "/api/auth/login",
   REGISTER: "/api/auth/register",
   LOGOUT: "/api/auth/logout",
-  CATEGORIES: "/api/categories",
+  CATEGORIES: "/api/common/categories",
+  EXPENSE_TYPES: "/api/common/expense_types",
+  EXPENSES: "/api/common/expenses",
 };

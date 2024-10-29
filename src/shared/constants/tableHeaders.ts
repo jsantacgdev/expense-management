7853dec589f2bc9a5f6db2dd7ffb12605dfb6fa7
JASantacruz00 <jsantacgdev@gmail.com>
2024-10-29 23:01:55 +0100

    Fix Table Headers to expenses columns name

--- src/shared/constants/tableHeaders.ts
+++ src/shared/constants/tableHeaders.ts
@@ -1,9 +1,9 @@
 import type { IColumn } from "@shared-interfaces/column";
 
 export const expensesTableHeaders: IColumn[] = [
-  { field: "nombre", header: "Nombre" },
+  { field: "nombre_gasto", header: "Nombre" },
   { field: "cantidad", header: "Cantidad (€)" },
   { field: "categoria", header: "Categoria" },
-  { field: "fecha", header: "Fecha" },
+  { field: "fecha_gasto", header: "Fecha" },
   { field: "tipo", header: "Tipo" },
 ];

3025cba388673a780029bee0e94c2650705f3db8
JASantacruz00 <jsantacgdev@gmail.com>
2024-10-31 14:03:03 +0100

    Update main view to user real expenses

--- src/modules/views/MainView.vue
+++ src/modules/views/MainView.vue
@@ -2,186 +2,38 @@
   <div class="grid grid-cols-2 w-screen h-full bg-blue-200">
     <div class="col-span-1 flex items-start justify-center mt-32 ml-12">
       <DataTable
-        :items="items"
+        :items="gastos"
         :columns="expensesTableHeaders"
         :striped-rows="true"
         class="w-4/5"
       />
     </div>
     <div class="col-span-1 flex items-start justify-center mt-32 ml-12">
-      <ChartData :data="items" />
+      <ChartData :data="gastos" />
     </div>
-    {{ categorias }}
   </div>
 </template>
 
 <script setup lang="ts">
-import type { ICategoria } from "@/shared/interfaces/common";
+import type {
+  ICategoria,
+  IGasto,
+  ITipoGasto,
+} from "@/shared/interfaces/common";
 import ChartData from "@shared/components/ChartData.vue";
 import DataTable from "@shared/components/DataTable.vue";
 import { expensesTableHeaders } from "@shared/constants/tableHeaders";
-import { useCategoryStore } from "@stores/commonStore";
+import { useCommonStore } from "@stores/commonStore";
+import { useAuthStore } from "@stores/authStore";
 import { ref, type Ref } from "vue";
 
-const items = [
-  {
-    id: 1,
-    cantidad: 30,
-    nombre: "Compra de comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Supermercados",
-  },
-  {
-    id: 2,
-    cantidad: 20,
-    nombre: "Gasolina",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Transportes",
-  },
-  {
-    id: 3,
-    cantidad: 10,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 4,
-    cantidad: 5,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 5,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 6,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 7,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 8,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 9,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 10,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 11,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 12,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 13,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 14,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 15,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 16,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 17,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 18,
-    cantidad: 2,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Comida",
-  },
-  {
-    id: 19,
-    cantidad: 200,
-    nombre: "Comida",
-    fecha: "01/01/2023",
-    tipo: "Gasto",
-    categoria: "Ropa",
-  },
-];
+const commonStore = useCommonStore();
+const authStore = useAuthStore();
 
-const categoryStore = useCategoryStore();
-const categorias: Ref<ICategoria[]> = ref([]);
+const usuario = authStore.getUser;
 
-categoryStore.categorias().then(() => {
-  categorias.value = categoryStore.getterCategorias;
+const gastos: Ref<IGasto[]> = ref([]);
+commonStore.gastos(usuario.id).then(() => {
+  gastos.value = commonStore.getterGastos;
 });
 </script>
